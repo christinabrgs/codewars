3ee0f51d736973ab5f25c1f5230d0fca6d56f9ba
Christina <christina.brgs@gmail.com>
2023-03-17 11:06:29 -0400

    3-17 codewars problem

--- week05.js
+++ week05.js
@@ -38,8 +38,9 @@ function countSheeps(arrayOfSheep) {
     }
     
     
-    /* Make a function that will return a greeting statement that uses an input; 
-    your program should return, */
+/* 3-16
+Make a function that will return a greeting statement that uses an input; 
+your program should return, */
     
     function greet(name){
   return `Hello, ${name} how are you doing today?`
@@ -47,3 +48,16 @@ function countSheeps(arrayOfSheep) {
 
 greet (console.log('What is your name?'))
     
+
+
+/* 3-17
+Complete the square sum function so that it squares each number 
+passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because */
+
+
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
