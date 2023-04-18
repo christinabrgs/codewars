af3dc839f46543a460ed76bbd5a93339bf8455b5
Christina <christina.brgs@gmail.com>
2023-04-18 14:49:13 -0400

    04-18

--- week10.js
+++ week10.js
@@ -4,3 +4,13 @@ function otherAngle(a, b) {
   let c = 180 - (a + b);
   return c;
 }
+
+// 04-18
+
+var summation = function (num) {
+ let sum = 0
+ for (let i = 0; i <= num; i++) {
+   sum += i
+ }
+ return sum
+}
