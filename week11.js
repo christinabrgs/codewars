a854c800fc7555611e1319f6d1c9b92d8696784e
Christina <christina.brgs@gmail.com>
2023-04-27 10:19:48 -0400

    04-27

--- week11.js
+++ week11.js
@@ -51,3 +51,11 @@ function outed(meet, boss){
 
   
 }
+
+ //  simple 
+
+function outed(meet, boss) {
+  let names = Object.keys(meet)
+  let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
+  return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+}
