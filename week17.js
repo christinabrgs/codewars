85a9369e5168221ebbffd7d66b394d162bc14d1b
Christina <christina.brgs@gmail.com>
2023-06-06 14:40:42 -0400

    06-06

--- week17.js
+++ week17.js
@@ -24,3 +24,44 @@ function duplicates(arr) {
   return duplicates;
   
 }
+
+
+// 06-06 
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+function rowSumOddNumbers(n) {
+ return n*n*n
+}
+
+
+
+
+
+
+
+
+function minAndMax(arr) {
+ sorted = arr.sort((a, b) => a-b)
+
+const min = sorted[0]
+const max = sorted[sorted.length - 0]
+
+return min, max
+}
