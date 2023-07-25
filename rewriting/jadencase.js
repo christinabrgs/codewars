f2c3074d2c2b48b939e5886a590ecfcb27488498
Christina <christina.brgs@gmail.com>
2023-07-24 20:37:01 -0400

    practice

---
+++ rewriting/jadencase.js
@@ -0,0 +1,37 @@
+const { string } = require("prop-types")
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').mapp(word => word[0].toUpperCase + word.substring(1)).join(' ')
+}
