460635298f596c17d5d4e06b746f1d77dac1a56a
Christina <christina.brgs@gmail.com>
2023-07-25 19:06:49 -0400

    make negative

---
+++ rewriting/makeNegative.js
@@ -0,0 +1,46 @@
+function makeNegative (num) {
+    if (num === 0 || num < 0) {
+        return num
+    }
+    else { return num * -1 }
+}
+
+function makeNegative(number) {
+    return number < 0 ? number : -number;
+}
+
+function makeNegative(number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
+
+function makeNegative (number) {
+    return number <= 0 ? number : -number
+}
